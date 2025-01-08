fb5805c44aa4b756a34cee8681eb7eab5efd4774
seyhmusoksak <seyhmusoksak@gmail.com>
2025-01-08 22:33:36 +0300

    Bug fixes of ex03

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 02:00:08 by soksak            #+#    #+#             */
-/*   Updated: 2024/11/22 02:21:24 by soksak           ###   ########.fr       */
+/*   Updated: 2025/01/08 22:32:37 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : humanName(name)
+HumanB::HumanB(std::string name) : humanName(name), weapon(NULL)
 {
 }
 
