9645f8d10cdfd9eaa3bad71b76bc20c692a04ab2
Şeyhmus Okşak <soksak@k1m05s09.42istanbul.com.tr>
2025-01-16 04:03:51 +0300

    Added cool main. func

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 02:39:39 by soksak            #+#    #+#             */
-/*   Updated: 2025/01/15 06:22:23 by soksak           ###   ########.fr       */
+/*   Updated: 2025/01/16 04:01:18 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ int main()
 	ClapTrap gwen("Gwen");
 	ClapTrap zed("Zed");
 
+	std::cout << "=======INITIAL STATUS==========" << std::endl;
 	gwen.showStatus("ClapTrap");
 	zed.showStatus("ClapTrap");
 
+	std::cout << "=======GWEN ATTACKS ZED==========" << std::endl;
 	gwen.attack(zed.getName());
 	zed.takeDamage(gwen.getAttackDamage());
 	zed.showStatus("ClapTrap");
-	gwen.beRepaired(10);
+	zed.beRepaired(10);
 
-
+	std::cout << "=======ZED ATTACKS GWEN==========" << std::endl;
 	zed.attack(gwen.getName());
 	gwen.takeDamage(zed.getAttackDamage());
 	gwen.showStatus("ClapTrap");
