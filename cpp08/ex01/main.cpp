badef0dee4cb42c83c967b2261445d1dd4f3efb8
Şeyhmus Okşak <soksak@k1m41s03.42istanbul.com.tr>
2025-07-25 02:57:34 +0300

    Added few more tests.

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 01:30:27 by soksak            #+#    #+#             */
-/*   Updated: 2025/07/19 18:06:12 by soksak           ###   ########.fr       */
+/*   Updated: 2025/07/25 02:56:07 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,26 @@
 
 int main()
 {
-	std::srand(static_cast<unsigned int>(std::time(NULL))); // Seed for random number generation
-	unsigned int randomNumber;
+	std::cout << "<------Subject Main------->" << std::endl;
 
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+
+	std::srand(std::time(0));
+	unsigned int randomNumber;
 	try
 	{
-		std::cout << "Creating a Span with 10000 elements." << std::endl;
+		std::cout << "<--- Creating a Span with 10000 elements. -->" << std::endl;
 		Span span(10000);
 		for (int i = 0; i < 10000; ++i)
 		{
-			randomNumber = rand() % 20000; // Generate a random number between 0 and 9999
-			std::cout << "Adding number: " << randomNumber << std::endl;
+			randomNumber = rand() % 10000;
 			span.addNumber(randomNumber);
 		}
 		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
@@ -36,6 +45,39 @@ int main()
 	{
 		std::cerr << e.what();
 	}
+
+	std::cout << "<-- Adding only one number -->" << std::endl;
+	try
+	{
+		Span sp3 = Span(10);
+		sp3.addNumber(1);
+		std::cout << sp3.shortestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "<-- Add more than 10 numbers with vector -->" << std::endl;
+	try
+	{
+		Span sp4 = Span(10);
+		std::vector<int> vec;
+		for (int i = 0; i < 10; ++i)
+		{
+			vec.push_back(i);
+		}
+		sp4.addNumber(vec.begin(), vec.end());
+		std::cout << "Shortest span: " << sp4.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << sp4.longestSpan() << std::endl;
+		std::cout << "Adding one more number:" << std::endl;
+		sp4.addNumber(5);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 
 	return 0;
 }
